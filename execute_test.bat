a59f7aa726c9feeb0d6599f0ba3779c256711699
miguelmazetto <33629994+miguelmazetto@users.noreply.github.com>
2026-05-01 10:35:32 -0300

    fix: Regex matchAll and split had issues compiling with msvc

--- execute_test.bat
+++ execute_test.bat
@@ -1,2 +1,3 @@
 @echo off
-test_package\.typoly_built\cpp\build\test_package.exe
+REM test_package\.typoly_built\cpp\build\test_package.exe
+test_package\.typoly_built\cpp\build\Debug\test_package.exe
